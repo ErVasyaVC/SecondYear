72d5f79d8cd978c48f401ff8119ac308a5332a96
Vasya Eremin <ervasyavc@gmail.com>
2024-11-15 00:00:40 +0300

    fghfffjhjh

--- BitField-2/Set.cpp
+++ BitField-2/Set.cpp
@@ -1,55 +1,63 @@
 #include "Set.h"
 
 
-Set::Set(size_t mp) : _bitField(10) {
-
+Set::Set(size_t mp) : _bitField(mp) {
+    _maxPower = mp;
 }
-Set::Set(const Set &s) : _bitField(10){
-
-} 
-Set::Set(const BitField &bf) : _bitField(10){
-
-}
+Set::Set(const Set &s) : _bitField(s._bitField), _maxPower(s.GetMaxPower()){} 
+Set::Set(const BitField &bf) : _bitField(bf), _maxPower(bf.GetLength()){}
 
 size_t Set::GetMaxPower() const{
-    return 0;
+    return _maxPower;
 }    
 void Set::InsElem(const uint64_t Elem){
-
+    _bitField.SetBit(Elem);
 }
 void Set::DelElem(const uint64_t Elem){
-
+    _bitField.ClrBit(Elem);
 }
 bool Set::IsMember(const uint64_t Elem) const{
-    return 0;
+    return _bitField.GetBit(Elem);
 }
 
 
 bool Set::operator== (const Set &s) const{
-    return 0;
+    return s._bitField == _bitField;
 }
 bool Set::operator!= (const Set &s) const{
-    return 0;
+    return (s._bitField == _bitField) == 0;
 }
 Set& Set::operator=(const Set &s){
+    _bitField = s._bitField;
+    _maxPower = s._maxPower;
     return *this;
 }
 Set Set::operator+ (const uint64_t Elem){
-    return *this;
+    Set s(*this);
+    s.InsElem(Elem);
+    return s;
 }
                                   
 Set Set::operator- (const uint64_t Elem){
-    return *this;
+    Set s(*this);
+    s.DelElem(Elem);
+    return s;
 }
                                    
 Set Set::operator+ (const Set &s){
-    return *this;
+    Set ss(max(s._maxPower, _maxPower));
+    ss._bitField = _bitField | s._bitField;
+    return ss;
 }
 Set Set::operator* (const Set &s){
-    return *this;
+    Set ss(max(s._maxPower, _maxPower));
+    ss._bitField = _bitField & s._bitField;
+    return ss;
 }
 Set Set::operator~ (){
-    return *this;
+    Set ss(_maxPower);
+    ss._bitField = ~_bitField;
+    return ss;
 }
 std::vector<uint64_t> Set::GetPrimary(){
     return std::vector<uint64_t>();
